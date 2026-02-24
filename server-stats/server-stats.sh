40e5ca4daaf5773def6a2826f89eaa6844528b8a
Ty Brown <58268784+imagineer-ty@users.noreply.github.com>
2026-02-23 23:14:35 -0500

    Add files via upload

--- server-stats/server-stats.sh
+++ server-stats/server-stats.sh
@@ -1,1 +1,19 @@
-
+#!/bin/bash
+echo "Memory Usage:"
+free -h | grep Mem | awk '{print "Total: ",$2,"Used:", $3, "Free:", $4}'
+echo ""
+echo "CPU Usage:"
+top -bn1 | grep "Cpu(s)" | \
+awk '{usage = 100 - $8; printf "Total CPU Usage: %.1f%%\n", usage}'
+echo ""
+echo "Disk Usage:"
+df -h | grep ' /$' | awk '{print "Total:", $2, "Used:", $3, "Free:", $4, "Usage:", $5}'
+echo ""
+echo "Top 5 Processes by CPU:"
+ps aux --sort=-%cpu | head -n 6
+echo ""
+echo "Top 5 Processes by Memory:"
+ps aux --sort=-%mem | head -n 6
+echo ""
+echo "OS Version:"
+grep PRETTY_NAME /etc/os-release | cut -d '=' -f2 | tr -d '"'
